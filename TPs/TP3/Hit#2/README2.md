6c4f6b7468ee191615d38befb21804f89d8881e8
matias <matiasherrneder@gmail.com>
2025-05-11 11:53:22 -0300

    estructurando

---
+++ TPs/TP3/Hit#2/README2.md
@@ -0,0 +1,11 @@
+* pararse en /terraform y hacer:
+```
+terraform init
+terraform plan
+terraform apply
+```
+
+* levantar los nodos:
+```
+
+```
---
+++ TPs/TP3/Hit#2/terraform/01-variables.tf
@@ -0,0 +1,25 @@
+variable "region" {
+  type    = string
+  default = "us-east1"
+}
+
+variable "zone" {
+  type    = string
+  default = "us-east1-b"
+}
+
+variable "credentials_file_path" {
+  description = "Path to GCP service account credentials file"
+  default     = "../credentials/credentials.json"
+}
+
+variable "project_id" {
+  type    = string
+  default = "automatic-rock-458016-q2"
+}
+
+variable "cluster_name" {
+  type        = string
+  description = "Nombre del cluster de Kubernetes"
+  default     = "sd-cluster"
+}
