bd94728c0c207c41c83983a933a2f85519c4d2d9
matias <matiasherrneder@gmail.com>
2026-01-04 18:34:35 -0300

    fix

--- TPs/TP5/Pilar2/pool/main.py
+++ TPs/TP5/Pilar2/pool/main.py
@@ -2,14 +2,8 @@ from threading import Thread
 from fastapi import FastAPI
 from pool import iniciar
 from endpoints import endpoints
-from starlette.middleware.proxy_headers import ProxyHeadersMiddleware
 
 app = FastAPI(title="Mining pool")
-
-app.add_middleware(
-    ProxyHeadersMiddleware,
-    trusted_hosts="*"
-)
 
 app.include_router(endpoints.router)
 
