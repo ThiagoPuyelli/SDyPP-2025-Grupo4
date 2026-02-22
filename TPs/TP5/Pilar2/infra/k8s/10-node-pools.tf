0b036b184fa7f568ccccadc8bc4c4830fd0ccd51
MatiasHerrneder <matiasherrneder@gmail.com>
2026-02-22 23:58:58 +0000

    add node pool blockchain

--- TPs/TP5/Pilar2/infra/k8s/10-node-pools.tf
+++ TPs/TP5/Pilar2/infra/k8s/10-node-pools.tf
@@ -29,6 +29,37 @@ resource "google_container_node_pool" "mineros" {
   }
 }
 
+resource "google_container_node_pool" "blockchain" {
+  name       = "blockchain"
+  cluster    = google_container_cluster.primary.id
+  node_count = 1
+
+  management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
+
+  autoscaling {
+    min_node_count = 1
+    max_node_count = 2
+  }
+
+  node_config {
+    preemptible  = false
+    machine_type = "e2-medium"
+    disk_size_gb = 20
+
+    labels = {
+      role = "blockchain"
+    }
+
+    service_account = google_service_account.kubernetes.email
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
+
 resource "google_container_node_pool" "infraestructura" {
   name       = "infraestructura"
   cluster    = google_container_cluster.primary.id
